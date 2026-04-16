a349aef6ac3c89a982a6854b0a7c6fa87d0c5530
Ihor <igorplahov25@gmail.com>
2026-04-16 11:57:05 +0300

    replace Sort_Controller with Search_Controller

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from Search_elements import *
 from File import *
 
 
-Sort = Sort_Controller()
+S = Sequential_Controller()
 
 print("Enter 1 to Sequential_Search\nEnter 2 to Fibonacci_Search\nEnter 3 Interpolation_Search")
 variant = input("Your choose: ")
 
 s = int(input("Enter the array size(100,1000): "))
 var = int(input("Enter the element which we search: "))
-Sort.Filling_array_random_elements(s)
-if Sort.Searching(variant, var):
+S.Filling_array_random_elements(s)
+if S.Searching(variant, var):
     print("Element is exist")
 else:
     print("Element is not exist")
-print(f"Array: {Sort.array}")
-print(f"Array: {Sort.history_list}")
+print(f"Array: {S.array}")
+print(f"Array: {S.history_list}")
